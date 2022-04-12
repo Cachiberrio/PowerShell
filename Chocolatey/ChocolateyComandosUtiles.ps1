8f58def778b6d554d207eda6113480727fda2cbc
am <am@tipsa.net>
2022-04-12 08:45:35 +0200

    ChocolateyComandosUtiles

---
+++ Chocolatey/ChocolateyComandosUtiles.ps1
@@ -0,0 +1,14 @@
+# Consulta de la lista de aplicaciones instaladas y controlables mediante Chocolatey
+Choco list -li
+
+# Búsqueda de aplicaciones para instalar
+Choco search <Nombre App>
+
+# Instalación de Apps (Si no se conoce el nombre del paquete, utilizar choco search)
+Choco install <Nombre Paquete>
+
+# Desinstalación de Apps (Si no se conoce el nombre del paquete, utilizar choco search)
+Choco uninstall <Nombre Paquete>
+
+# Actualización de la App (Si no se conoce el nombre del paquete, utilizar choco search)
+Choco Upgrade <Nombre paquete> 
